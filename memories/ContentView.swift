64ef9b5ae64999d5187dd3d7ae2f015739dfe048
heartsetGirl2y <382914340@qq.com>
2021-01-18 19:54:31 +0800

    Initial Commit

---
+++ memories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  memories
+//
+//  Created by 李剑澜 on 2021/1/18.
+//  Copyright © 2021 Lan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
