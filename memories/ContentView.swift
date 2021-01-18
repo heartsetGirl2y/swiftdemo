d6dfd3623b6d0beef83e17598b073c4e25d52d20
heartsetGirl2y <382914340@qq.com>
2021-01-18 23:12:03 +0800

    init

--- memories/ContentView.swift
+++ memories/ContentView.swift
@@ -8,11 +8,65 @@
 
 import SwiftUI
 
+struct cardcontent {
+    var emoji:String;
+    var isFaceup:Bool;
+    var id:Int;
+}
+//class
 struct ContentView: View {
+    let emojiStructList:[cardcontent]=[cardcontent(emoji: "🤯", isFaceup: true,id:0),cardcontent(emoji: "😬", isFaceup: false,id:1),cardcontent(emoji: "🧐", isFaceup: true,id:2),cardcontent(emoji: "🤣", isFaceup: true,id:3)]
+//    let emojiList:[String]=["🤯","😬","🧐","🤣"]
     var body: some View {
-        Text("Hello, World!")
+        HStack(content: {
+//            ForEach(emojiList,id:\.self) { emoji in
+//                cardView(emoji:emoji)
+//            }
+            ForEach(emojiStructList, id:\.id, content: {
+                emoji in cardView(emoji: emoji.emoji,isFaceup: emoji.isFaceup)
+            })
+        }).padding()
+            .foregroundColor(Color("maincolor"))
+            .font(Font.largeTitle)
+
     }
 }
+struct cardView:View{
+    let emoji:String;
+    let isFaceup:Bool;
+    var body: some View{
+        ZStack(content: {
+            if isFaceup {
+            RoundedRectangle(cornerRadius: 10.0).fill(Color.white);
+            RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0)
+            Text(emoji);
+            }else{
+                RoundedRectangle(cornerRadius: 10.0).fill(Color("maincolor"));
+            }
+        })
+    }
+}
+
+//myself
+
+//struct ContentView:View{
+//    var body:some View{
+//        blockArea(str: "🤯")
+//    }
+//}
+//
+//struct blockArea:View {
+//     let str:String
+//    var body:some View{
+//        ZStack(content: {
+//            RoundedRectangle(cornerRadius: 10.0).fill(Color.white);
+//            RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 3)
+//            Text(str)
+//        })
+//            .font(Font.largeTitle)
+//            .foregroundColor(Color("maincolor"))
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
